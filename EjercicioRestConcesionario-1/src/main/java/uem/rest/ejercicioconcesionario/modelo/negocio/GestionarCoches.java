ac17ba602cfea55db5c59b48496a81e06a61a539
Albersss <alber.garcia.lopez106@gmail.com>
2021-03-08 12:15:54 +0100

    finish conce

--- EjercicioRestConcesionario-1/src/main/java/uem/rest/ejercicioconcesionario/modelo/negocio/GestionarCoches.java
+++ EjercicioRestConcesionario-1/src/main/java/uem/rest/ejercicioconcesionario/modelo/negocio/GestionarCoches.java
@@ -1,6 +1,7 @@
 package uem.rest.ejercicioconcesionario.modelo.negocio;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,5 +19,40 @@ public class GestionarCoches {
 		List<Coche> lista = daocoche.findAll();
         return lista;
 	}
+	public Coche altaCoche(Coche coche) {
+		if(coche.getPotencia()<0 || daocoche.findById(coche.getId()).isPresent()) {
+			return null;
+		}
+		Coche c = daocoche.save(coche);
+		return c;
+	}
+	
+	public Coche obtener(int id) {
+		Optional<Coche> opt = daocoche.findById(id);
+		if(opt.isPresent()) {
+			return opt.get();
+		}else {
+			return null;
+		}
+	}
+	
+	public Coche modificar(Coche c) {
+		if(c.getPotencia()<0) {
+			return null;
+		}
+		Coche coche = daocoche.save(c);
+		return coche;
+	}
+	
+
+	public boolean borrarCoche(int id) {
+		if(daocoche.findById(id).isPresent()){
+			daocoche.deleteById(id);
+			return true;
+		}
+		return false;
+	}
+
+
 
 }
